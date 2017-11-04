933ecc621320b6b84b08c25a3ed3a11ba21b8784
Alma Chelouche <alma.chelouche@gmail.com>
2017-11-04 16:54:16 +0800

    New error

--- Pitch perfect/PlaySoundsViewController.swift
+++ Pitch perfect/PlaySoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
    
@@ -18,6 +19,16 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var birdButton: UIButton!
     @IBOutlet weak var soundWavesButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
     
     var recordAudioURL : URL!
     
@@ -44,24 +55,12 @@ class PlaySoundsViewController: UIViewController {
         soundWavesButton.imageView?.contentMode = .scaleAspectFit
         StopButton.contentMode = .center
         StopButton.imageView?.contentMode = .scaleAspectFit
+        setupAudio()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
